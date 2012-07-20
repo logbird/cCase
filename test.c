70731e2552c7f15fb268cf9e7173dc377d46a374
logbird <logbird@126.com>
2012-07-20 17:41:41 +0800

    测试 lseek函数

--- test.c
+++ test.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
-    int num[6] = {1,43,23,62,123};
+    int f = open("b", O_CREAT | O_RDONLY | O_WRONLY);
+    write(f, "logbird000", 10);
+    lseek(f, 10, SEEK_SET);
+    write(f, "\nlogbird000", 10);
+    lseek(f, 100, SEEK_END);
+    write(f, "\nend", 10);
+    close(f);
     return 1;
 }
 
